c7869c13077de13cc84b413b19f909480826ae01
putkte <tero.putkonen@tiimi.org>
2010-09-24 08:11:27 +0300

    added logging to runlog-folder and some checks on generate_everything.sh

--- generate_everything.sh
+++ generate_everything.sh
@@ -1,13 +1,28 @@
-#!/bin/sh
-
-mkdir output 2>/dev/null
-
-for x in logs/*; do
-	lib/log-to-html.pl "$x"
-	mv -v $x.html output/
-done
-
-lib/index.pl
-
-cp ressources/* output
-
+#!/bin/bash
+if [ ! -d runlog ]; then
+	mkdir runlog
+fi
+if [ -d output ]; then
+    MKD=1; 
+else  
+	mkdir output;
+    MKD=1; 
+fi
+FTIME=`date +%Y-%m-%d-%H.%m.%S`;
+RUNLOG="$FTIME-runlog";
+if [ -d logs ]; then 
+    
+	for x in logs/*; do
+		if [ -f runlog/"$RUNLOG" ]; then 
+			lib/log-to-html.pl "$x" >>runlog/"$RUNLOG";
+		else 
+			lib/log-to-html.pl "$x" >runlog/"$RUNLOG";
+		fi
+		mv -v $x.html output/;
+	done;
+   CONVERT=1;
+fi
+if [ $MKD -eq 1 -a $CONVERT -eq 1 ]; then
+	lib/index.pl >>runlog/"$RUNLOG"
+	cp ressources/* output
+fi
